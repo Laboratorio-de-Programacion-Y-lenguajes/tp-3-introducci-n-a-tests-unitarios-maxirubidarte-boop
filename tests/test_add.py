48a9b55021a5716b5aed2c8680c865ea3c47511b
maxirubidarte-boop <maxirubidarte@gmail.com>
2026-04-15 19:40:59 -0300

    feat: test suma finalizado

--- tests/test_add.py
+++ tests/test_add.py
@@ -10,6 +10,16 @@ def test_add_suma_positivos():
     """Ejemplo: 1 + 2 debe dar 3."""
     assert add(1, 2) == 3
 
+    
+@pytest.mark.parametrize("a, b, esperado", [
+    (-1, -2, -3),      # Caso negativos
+    (5, -3, 2),        # Caso pos/neg
+    (7, 0, 7),         # Caso con cero
+    (1.5, 2.5, 4.0)    # Caso decimales
+])
+def test_add_parametrizado(a, b, esperado):
+     assert add(a, b) == esperado
+
 
 # --- TU TURNO ---
 # Agregá tests para los siguientes casos:
